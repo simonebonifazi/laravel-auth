a6b95f11a8210bb1163268dfe0896c875710d439
simonebonifazi <simonbonifraz@gmail.com>
2022-09-27 17:29:55 +0200

    index's view added

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,43 @@
+@extends('layouts.app')
+
+@section('content')
+<header>
+    <h2> I tuoi post </h2>
+</header>
+
+
+<table class="table">
+    <thead class="thead-light">
+        <tr>
+            <th scope="col">#</th>
+            <th scope="col">Titolo</th>
+            <th scope="col">Slug</th>
+            <th scope="col">Creato alle:</th>
+            <th scope="col">Ultima modifica:</th>
+            <th scope="col">Azioni ... </th>
+        </tr>
+    </thead>
+    <tbody>
+        @forelse($posts as $post)
+        <tr>
+            <th scope="row">{{ $post->id }}</th>
+            <td>{{$post->title}}</td>
+            <td>{{$post->slug}}</td>
+            <td>{{$post->created_at}}</td>
+            <td>{{$post->updated_at}}</td>
+            <td></td>
+        </tr>
+        @empty
+        <tr>
+            <td colspan="6">
+                <h4 class="text-center">
+
+                    Ancora nessun post
+                </h4>
+            </td>
+        </tr>
+        @endforelse
+
+    </tbody>
+</table>
+@endsection
