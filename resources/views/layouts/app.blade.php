d3581e80ee56ca45c5b45046d4d4d710d17b00e6
simonebonifazi <simonbonifraz@gmail.com>
2022-09-27 17:36:53 +0200

    added container on index & FA on app.blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,6 +10,11 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
+    <!-- FA -->
+    <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css'
+        integrity='sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A=='
+        crossorigin='anonymous' />
+
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}" defer></script>
 
