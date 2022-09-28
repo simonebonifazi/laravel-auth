16d02476e4fea8578b29d82cd11dc161347e1580
simonebonifazi <simonbonifraz@gmail.com>
2022-09-28 23:56:33 +0200

    added on post seeder category_id(s)

--- database/seeds/PostSeeder.php
+++ database/seeds/PostSeeder.php
@@ -2,8 +2,10 @@
 
 use Illuminate\Database\Seeder;
 use App\Models\Post;
+use App\Models\Category;
 use Faker\Generator as Faker;
 use Illuminate\Support\Str;
+use Illuminate\Support\Arr;
 
 class PostSeeder extends Seeder
 {
@@ -13,11 +15,14 @@ class PostSeeder extends Seeder
      * @return void
      */
     public function run( Faker $faker )
-    {
+    {               
+        $category_ids = Category::pluck('id')->toArray();
+
         for($i = 0; $i <10; $i++){
             $new_post = new Post();
 
             $new_post->title = $faker->text(20);
+            $new_post->category_id = Arr::random($category_ids);
             $new_post->content = $faker->paragraphs(2, true);
             $new_post->image = $faker->imageUrl(150, 150);
             $new_post->slug = Str::slug($new_post->title, '-');
