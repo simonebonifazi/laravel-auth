6d294be1a2f8634ea467ca1acb6ca7c8655e8a6e
simonebonifazi <simonbonifraz@gmail.com>
2022-09-30 18:11:11 +0200

    migration compiled

--- resources/js/admin/extra/image_preview.js
+++
@@ -1,7 +0,0 @@
-const url = "https://image.shutterstock.com/image-vector/ui-image-placeholder-wireframes-apps-260nw-1037719204.jpg";
-const preview = document.getElementById('preview');
-const imageField = document.getElementById('image');
-imageField.addEventListener('input', ()=>{
-
-   preview.src = url || imageField.value;
-})
